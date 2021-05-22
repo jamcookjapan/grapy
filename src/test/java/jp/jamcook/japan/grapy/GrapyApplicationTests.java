b23d4689fafcb4248895087119efc9959861dc33
jamcookjapan <jamcook.japan@gmail.com>
2021-05-22 11:33:31 +0900

    fc

---
+++ src/test/java/jp/jamcook/japan/grapy/GrapyApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.jamcook.japan.grapy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrapyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
